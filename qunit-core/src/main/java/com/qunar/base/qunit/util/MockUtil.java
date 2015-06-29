00b45127b385379eb59f0e99d55b3a8794937260
Zhaohui Yu <yuyijq@gmail.com>
2015-06-29 12:10:39 +0800

    removed main

--- qunit-core/src/main/java/com/qunar/base/qunit/util/MockUtil.java
+++ qunit-core/src/main/java/com/qunar/base/qunit/util/MockUtil.java
@@ -1,13 +1,10 @@
 package com.qunar.base.qunit.util;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.JSONSerializer;
 import com.alibaba.fastjson.serializer.SerializeConfig;
 import com.alibaba.fastjson.serializer.SerializeWriter;
 import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.qunar.base.qunit.objectfactory.ObjectFactory;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeSet;
@@ -49,44 +46,4 @@ public class MockUtil {
         }
     }
 
-    public static void main(String[] args) {
-        HashMap<Long, Object> map = new HashMap<Long, Object>();
-        Hotel v = new Hotel();
-        v.setName("aaa");
-        v.setPrice(1.0);
-        map.put(1L, v);
-
-        map.put(2L, Color.a);
-
-        String s = MockUtil.toJson(map);
-        System.out.println(s);
-        Object parse = JSON.parse(s);
-
-
-        System.out.println(parse);
-        System.out.println(s);
-
-    }
-
-    private static class Hotel {
-        private String name;
-
-        private double price;
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public double getPrice() {
-            return price;
-        }
-
-        public void setPrice(double price) {
-            this.price = price;
-        }
-    }
 }
